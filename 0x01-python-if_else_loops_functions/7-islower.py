cbd7a58478dec743379975ce872c83b2e8412a16
MichaelAuditore <michael_v613@hotmail.com>
2019-12-10 18:14:26 +0000

    [Fix] Comments for a function added

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,12 @@
 def islower(c):
+    '''
+    Validate is c is uppercase or lowercase
+
+    :param char c: Letter to validate
+    :type c: character or None
+    :return: True or false
+    :rtype: bool
+    '''
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
