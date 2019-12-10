a63701dd7ffa45a40089760b852c7c16fef6e9c8
MichaelAuditore <michael_v613@hotmail.com>
2019-12-10 18:41:44 +0000

    [Fix] added the shebang for execute in python3

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
+#!/usr/bin/python3
 def islower(c):
-    # Validate if a letter is upper or lower
-    #
-    # Return True is lowercase or False is upper
-    if ord(c) >= 97 and ord(c) <= 122:
+    if (ord(c) >= 97) and (ord(c) <= 122):
         return True
     else:
         return False
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_last_digit(number):
     number = (number * -1) % 10 if number < 0 else number % 10
     print("{:d}".format(number), end="")
