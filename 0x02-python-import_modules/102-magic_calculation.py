540fd8fac93e5670461d49cfeab74f28310f00a4
MichaelAuditore <michael_v613@hotmail.com>
2019-12-12 18:03:03 +0000

    [Fix] pattern lines

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,4 @@
+#!/usr/bin/python3
 __import__ magic_calculation_102
-from add import add
-from sub import sub
-def magic_calculation(a, b):
-    if a < b:
-        c = add(a, b)
-        for i in range(c, 90):
-            c = add(c , i)
-        return c
-    return sub(a, b)
+print(add(3,4))
+print(sub(3,4))
