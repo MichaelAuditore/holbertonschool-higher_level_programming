47ecbbcff1e5a06140e36d19e12454ae3bf09afa
MichaelAuditore <michael_v613@hotmail.com>
2019-12-12 16:47:23 +0000

    [Fix] PEP8 style

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     isOperator = False
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     for i in operators:
         if sys.argv[2] == i:
-            isOperator = True;
-            break;
-    if isOperator == False:
+            isOperator = True
+            break
+    if isOperator is False:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if (sys.argv[2] == '+'):
