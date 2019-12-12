32ecfd4fd31d2435012323189e7ba87e5e7fce94
MichaelAuditore <michael_v613@hotmail.com>
2019-12-12 17:52:38 +0000

    [Fix] conditions

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+__import__ magic_calculation_102
+from add import add
+from sub import sub
 def magic_calculation(a, b):
-    __import__ magic_calculation_102
-    from add import add
-    from sub import sub
     if a < b:
         c = add(a, b)
         for i in range(c, 90):
