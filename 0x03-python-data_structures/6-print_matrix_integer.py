acd3a7cfba27a8b325f583bfa87314cbe2c58e92
MichaelAuditore <michael_v613@hotmail.com>
2019-12-17 17:11:57 +0000

    [New file] print a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    idx = 0
+    for i in matrix:
+        for a in i:
+            if a != i[-1]:
+                print("{:d}".format(a), end=" ")
+            else:
+                print("{:d}".format(a))
