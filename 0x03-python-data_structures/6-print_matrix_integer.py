eb8505bd22e7853659a685aa402844c0aa73988a
MichaelAuditore <michael_v613@hotmail.com>
2019-12-17 17:26:31 +0000

    [Fix] validating is matrix is NULL

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    idx = 0
-    for i in matrix:
-        for a in i:
-            if a != i[-1]:
-                print("{:d}".format(a), end=" ")
-            else:
-                print("{:d}".format(a), end="")
-            print()
+    if matrix is None:
+        print()
+    else:
+        idx = 0
+        for i in matrix:
+            for a in i:
+                if a != i[-1]:
+                    print("{:d}".format(a), end=" ")
+                else:
+                    print("{:d}".format(a))
