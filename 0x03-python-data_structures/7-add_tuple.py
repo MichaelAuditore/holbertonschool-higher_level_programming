26b29aea13b8da3d547ca483131784b197680dea
MichaelAuditore <michael_v613@hotmail.com>
2019-12-17 18:09:57 +0000

    [Fix] statements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = (0, 0)
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_b[1] + tuple_a[1])
-    elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        tuple_c = (tuple_a[0] + tuple_b[0], )
-    elif len(tuple_a) >= 2 and len(tuple_b) == 1:
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1])
-    elif len(tuple_a) >= 2 and len(tuple_b) == 0:
-        tuple_c = (tuple_a[0], tuple_a[1])
-    elif len(tuple_b) >= 2 and len(tuple_a) == 0:
-        tuple_c = (tuple_b[0], tuple_b[1])
+    sum1 = 0
+    sum2 = 0
+
+    if (len(tuple_a) == 0):
+        a = 0
+        b = 0
+    elif (len(tuple_a) == 1):
+        a = tuple_a[0]
+        b = 0
+    else:
+        a = tuple_a[0]
+        b = tuple_a[1]
+    if (len(tuple_b) == 0):
+        aa = 0
+        bb = 0
+    elif (len(tuple_b) == 1):
+        aa = tuple_b[0]
+        bb = 0
+    else:
+        aa = tuple_b[0]
+        bb = tuple_b[1]
+    sum1 = a + aa
+    sum2 = b + bb
+    tuple_c = (sum1, sum2)
     return tuple_c
