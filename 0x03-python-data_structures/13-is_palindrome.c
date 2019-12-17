249c3976403c02561d9c76ca6c7ea41da924c3ba
MichaelAuditore <michael_v613@hotmail.com>
2019-12-17 15:40:13 +0000

    [New file] Catch is a linked list is palindrome or not

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * is_palindrome - catch if a linked list is palindrome or not
+ * @head: Pointer to the first node
+ * Return: 1 is palindrome or 0 is not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *header;
+	listint_t *prevtail;
+	listint_t *tail;
+	int length = 0;
+	int pos;
+
+	if (*head == NULL || head == NULL)
+		return (1);
+	header = *head;
+	tail = *head;
+	while (tail->next != NULL)
+	{
+		tail = tail->next;
+		length++;
+	}
+	while (header != NULL)
+	{
+		if (header->n == tail->n && length == 0)
+			return (1);
+		if (header->n == tail->n)
+		{
+			header = header->next;
+			prevtail = *head;
+			pos = 1;
+			while (pos < length)
+			{
+				prevtail = prevtail->next;
+				pos++;
+			}
+			length--;
+			tail = prevtail;
+		}
+		else
+			break;
+	}
+	return (0);
+}
