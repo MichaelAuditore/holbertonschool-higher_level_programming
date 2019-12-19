d73df76230f739c001ff1ad102092cd8ed16675f
MichaelAuditore <michael_v613@hotmail.com>
2019-12-19 22:12:40 +0000

    [Fix] Solving using lists

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,43 +5,22 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
     else:
-        number = 0
-        prev = 0
-        for i in roman_string:
-            isNumber = True
-            if i == 'I':
-                number += 1
-            elif i == 'V' and prev == 1:
-                number += 5 - 2
-                isNumber = False
-            elif i == 'X' and prev == 1:
-                number += 10 - 2
-                isNumber = False
-            elif i == 'L' and prev == 1:
-                number += 50 - 2
-                isNumber = False
-            elif i == 'C' and prev == 1:
-                number += 100 - 2
-                isNumber = False
-            elif i == 'D' and prev == 1:
-                number += 500 - 2
-                isNumber = False
-            elif i == 'M' and prev == 1:
-                number += 1000 - 2
-                isNumber = False
-            else:
-                if isNumber:
-                    if i == 'V':
-                        number += 5
-                    elif i == 'X':
-                        number += 10
-                    elif i == 'L':
-                        number += 50
-                    elif i == 'C':
-                        number += 100
-                    elif i == 'D':
-                        number += 500
-                    elif i == 'M':
-                        number += 1000
-            prev = number
-        return number
+        roman_numbers = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        int_values = [1, 5, 10, 50, 100, 500, 1000]
+        newlist = list(roman_string)
+        for n in range(0, len(newlist)):
+            for r in range(0, len(roman_numbers)):
+                if newlist[n] == roman_numbers[r]:
+                    newlist[n] = int_values[r]
+                    break
+        if len(newlist) == 1:
+            return newlist[0]
+        else:
+            add = 0
+            for n in range(1, len(newlist)):
+                if newlist[n - 1] >= newlist[n]:
+                    add += newlist[n - 1]
+                else:
+                    add -= newlist[n - 1]
+            add += newlist[n]
+        return add
