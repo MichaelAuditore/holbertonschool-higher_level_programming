e4bc02d73b0828bdcfdc1c674dcb0e3548645046
MichaelAuditore <michael_v613@hotmail.com>
2019-12-19 18:34:28 +0000

    [Fix] Output fixed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new = {}
-    if len(a_dictionary) != 0:
-        OrderByKeys = sorted(a_dictionary.keys())
-        for i in OrderByKeys:
-            new[i] = a_dictionary[i] * 2
-        return new
+    for i in OrderByKeys:
+        new[i] = a_dictionary[i] * 2
+    return new
