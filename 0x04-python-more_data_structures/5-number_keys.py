7c18d96e081be40679f12c907602e54b728a6a24
MichaelAuditore <michael_v613@hotmail.com>
2019-12-19 17:56:27 +0000

    [New file] function who returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    if a_dictionary is None:
+        return 0
+    else:
+        add = 0;
+        for i in a_dictionary:
+            add += 1
+        return add
