7e4723271f189e21bb3f9f5c9cdcee4ce3eeee49
MichaelAuditore <michael_v613@hotmail.com>
2019-12-19 19:33:47 +0000

    [New file] convert a roman number into integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    else:
+        number = 0
+        prev = 0
+        for i in roman_string:
+            isNumber = True
+            if i == 'I':
+                number += 1
+            elif i == 'V' and prev == 1:
+                number += 5 - 2
+                isNumber = False
+            elif i == 'X' and prev == 1:
+                number += 10 - 2
+                isNumber = False
+            elif i == 'L' and prev == 1:
+                number += 50 - 2
+                isNumber = False
+            elif i == 'C' and prev == 1:
+                number += 100 - 2
+                isNumber = False
+            elif i == 'D' and prev == 1:
+                number += 500 - 2
+                isNumber = False
+            elif i == 'M' and prev == 1:
+                number += 1000 - 2
+                isNumber = False
+            else:
+                if isNumber:
+                    if i == 'V':
+                        number += 5
+                    elif i == 'X':
+                        number += 10
+                    elif i == 'L':
+                        number += 50
+                    elif i == 'C':
+                        number += 100
+                    elif i == 'D':
+                        number += 500
+                    elif i == 'M':
+                        number += 1000
+            prev = number
+        return number
