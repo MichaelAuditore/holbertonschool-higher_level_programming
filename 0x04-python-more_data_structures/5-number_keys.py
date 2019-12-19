2b6a475d1720528db4d641b02268cf252642bb59
MichaelAuditore <michael_v613@hotmail.com>
2019-12-19 17:57:14 +0000

    [Fix] PEP8 Style

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,7 +3,7 @@ def number_keys(a_dictionary):
     if a_dictionary is None:
         return 0
     else:
-        add = 0;
+        add = 0
         for i in a_dictionary:
             add += 1
         return add
