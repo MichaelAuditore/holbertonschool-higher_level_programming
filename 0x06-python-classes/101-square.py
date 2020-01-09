60194e096fa53712dd2084e6737e3934642f57bc
MichaelAuditore <michael_v613@hotmail.com>
2020-01-09 19:24:06 +0000

    [New file] returning the text to print for a Square function

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.position = position
+        self.size = size
+
+    def area(self):
+       return self.__size * self.__size
+
+    def __str__(self):
+        pr = ""
+        if (self.size == 0):
+            return pr
+        else:
+            pos = self.__position[1]
+            while (pos > 0):
+                pr += "\n"
+                pos -= 1
+            x, y = self.__size, self.__size
+            for i in range(x):
+                nspaces = self.__position[0]
+                for j in range(y):
+                    while(nspaces > 0):
+                        pr += " "
+                        nspaces -= 1
+                    pr += "#"
+                if (i < self.size - 1):
+                    pr += "\n"
+            return pr
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            pos = self.__position[1]
+            while (pos > 0):
+                print()
+                pos -= 1
+            x, y = self.__size, self.__size
+            for i in range(x):
+                nspaces = self.__position[0]
+                for j in range(y):
+                    while(nspaces > 0):
+                        print(" ", end="")
+                        nspaces -= 1
+                    print("#", end="")
+                print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__size
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            for val in value:
+                if not isinstance(val, int) or val < 0:
+                    raise TypeError("position must be a tuple of 2 " +
+                                    "positive integers")
+            self.__position = value
