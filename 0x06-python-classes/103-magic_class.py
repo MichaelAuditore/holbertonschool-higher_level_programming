f77d02106032f35bb8053c127db82b4c7da30b05
MichaelAuditore <michael_v613@hotmail.com>
2020-01-09 21:22:29 +0000

    [Fix] PEP8 Style

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,14 +4,16 @@ import math
 
 class MagicClass:
 
+    """ This Class stores the data of a circle
+    to get the area and circunference"""
     def __init__(self, radius=0):
         self.__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
-
+    """ function to get the area """
     def area(self):
         return (self.__radius ** 2) math.pi
-
+    """ function to get the circumference of the radius of circle"""
     def circumference(self):
         return 2 * math.pi * self.__radius
