59437b708e2c28fa01e7ce3e6dae4f82f87a1160
MichaelAuditore <michael_v613@hotmail.com>
2020-01-14 19:52:33 +0000

    [New file] Class with area function and perimeter function for rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """
+    Class that defines a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        return ((self.__height * 2) + (self.__width * 2))
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("heigth must be an integer")
+        if value < 0:
+            raise ValueError("heigth must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
