19d91599eb1fd35d8f54a66c9a490b129a3ecc96
MichaelAuditore <michael_v613@hotmail.com>
2020-01-15 19:45:11 +0000

    [Files for each answer]

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(n=[-1]):
+    n[0] += 1
+    return ("Holberton" + ", Holberton" * n[0])
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class LockedClass:
+    """
+    New class who accepts only a parameter called first_name,
+    base off into the memory assigned to the class
+    """
+    __slots__ = 'first_name'
