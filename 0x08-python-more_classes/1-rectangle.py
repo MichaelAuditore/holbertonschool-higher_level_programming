0a41578ec095dd625cca3c8cb43b95fa77a19d91
Miguel Angel Parada Cañon <michael_v613@hotmail.com>
2020-01-15 19:25:33 -0500

    Fix
    Output error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,22 +8,14 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
+        
+    @property
+    def width(self):
+        return (self.__width)
 
     @property
     def height(self):
         return (self.__height)
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("heigth must be an integer")
-        if (value < 0):
-            raise ValueError("heigth must be >= 0")
-        self.__height = value
-
-    @property
-    def width(self):
-        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -32,3 +24,11 @@ class Rectangle:
         if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
+        
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
