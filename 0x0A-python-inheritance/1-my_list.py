650976d4f3c8c2cde7a20c3c9fe6475cd6c43dc3
MichaelAuditore <michael_v613@hotmail.com>
2020-01-20 20:58:52 +0000

    [New file] Sort a List Object

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+class MyList(list):
+    """Class list inherits list"""
+    def print_sorted(self):
+        new = MyList()
+        new = self.copy()
+        new.sort()
+        print(new)
