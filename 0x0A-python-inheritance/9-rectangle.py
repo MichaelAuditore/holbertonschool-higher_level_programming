a1f60e63361163b3411ba93ade02b61fab746626
MichaelAuditore <michael_v613@hotmail.com>
2020-01-20 23:26:40 +0000

    [New file] A Full Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    Class to define if the object have Base Geometry
+    """
+    def area(self):
+        """
+        define the area for a geometry object
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        define a validator for a value will be used into the function
+        """
+        if (type(value) is not int):
+            raise TypeError(name + ' must be an integer')
+        if (value <= 0):
+            raise ValueError(name + ' must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class to define a rectangle as BaseGeometry
+    """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        s = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return s
