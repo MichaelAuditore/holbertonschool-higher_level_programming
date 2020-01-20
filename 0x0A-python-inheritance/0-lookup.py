9906203f8294dc497262383a03e320f1ce44cb9c
MichaelAuditore <michael_v613@hotmail.com>
2020-01-20 20:58:37 +0000

    [New file] show all attr and  available methods for a class

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+def lookup(obj):
+    """
+    function that returns the list of available attributes
+    and methods of an object
+    """
+    ls = []
+    for att in dir(obj):
+        ls.append(att)
+    return ls
