8309eae9c8c4795ae303c95c32c5df91eb66fa3e
MichaelAuditore <michael_v613@hotmail.com>
2020-01-21 18:32:41 +0000

    [Fix] Description for a class

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -2,6 +2,9 @@
 
 
 class Student:
+    """
+    Class to define a student
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
