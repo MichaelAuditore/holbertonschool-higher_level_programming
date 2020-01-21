4aed3b3386e7762726609eefd54a92d5a56d7ea2
MichaelAuditore <michael_v613@hotmail.com>
2020-01-21 18:16:54 +0000

    [Fix] Pycodestyle

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,7 +2,7 @@
 import sys
 import os
 save_to_json = __import__('7-save_to_json_file').save_to_json_file
-load_from =  __import__('8-load_from_json_file').load_from_json_file
+load_from = __import__('8-load_from_json_file').load_from_json_file
 
 if os.path.exists("add_item.json") is True:
     lst = list(load_from("add_item.json"))
