49dbf83f0c22b1f707f2bf11edaf44924ca26254
MichaelAuditore <michael_v613@hotmail.com>
2020-01-22 23:20:00 +0000

    [Fix] Dump when a file is open to copy object into

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -7,7 +7,6 @@ def save_to_json_file(my_obj, filename):
     save an object into text file in JSON representation
     Returns nothing
     """
-    x = json.dumps(my_obj)
     with open(filename, "w") as f:
-        f.write(x)
+        json.dump(my_obj, f)
     f.closed
