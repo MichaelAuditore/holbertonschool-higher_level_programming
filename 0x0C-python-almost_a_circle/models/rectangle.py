d85d70f72c0e82529bfdfce6e99750ab01443249
MichaelAuditore <michael_v613@hotmail.com>
2020-01-27 19:12:22 +0000

    [Fix] PEP8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Class to define a rectangle
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Constructor of the class Rectangle
