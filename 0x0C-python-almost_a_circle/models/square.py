ee645aacbaf6bda7d4806e83df46790dd92a8bcc
MichaelAuditore <michael_v613@hotmail.com>
2020-01-27 19:13:56 +0000

    [Fix] PEP8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,6 +4,10 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Class to define a Square
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor for the class Square"""
         super().__init__(size, size, x, y, id)
