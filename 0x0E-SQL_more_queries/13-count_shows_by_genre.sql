1908a667c2e75da232f45369a1efcf3f76a42dcb
MichaelAuditore <michael_v613@hotmail.com>
2020-03-03 19:58:30 +0000

    [Fix] Name of columns Fixed

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- script that lists all genres from hbtn_0d_tvshows
 --  and displays the number of shows linked to each.
-SELECT g.name, COUNT(tv_g.show_id) AS number_of_shows FROM tv_shows AS tv_s
+SELECT g.name AS genre, COUNT(tv_g.show_id) AS number_of_shows FROM tv_shows AS tv_s
 RIGHT JOIN tv_show_genres AS tv_g ON tv_s.id = tv_g.show_id
 RIGHT JOIN tv_genres AS g ON tv_g.genre_id = g.id
 GROUP BY g.name
