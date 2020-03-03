1a113649816a42bc18a5161b92bc1122741372f6
MichaelAuditore <michael_v613@hotmail.com>
2020-03-03 21:00:04 +0000

    [Fix] JOIN Implementation from Genres to shows

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- script that lists all genres from hbtn_0d_tvshows
 --  and displays the number of shows linked to each.
-SELECT g.name FROM tv_shows AS tv_s
-LEFT JOIN tv_show_genres AS tv_g ON tv_s.id = tv_g.show_id
-LEFT JOIN tv_genres AS g ON tv_g.genre_id = g.id
-WHERE tv_s.title='Dexter';
+SELECT genres.name FROM tv_genres AS genres
+JOIN tv_show_genres AS show_genres ON show_genres.genre_id = genres.id
+JOIN tv_shows AS shows ON shows.id = show_genres.show_id
+WHERE shows.title='Dexter';
