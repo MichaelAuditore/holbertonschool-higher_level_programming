c0734669172f805261da9d8a253467997b317ce7
MichaelAuditore <michael_v613@hotmail.com>
2020-03-03 20:27:14 +0000

    [Fix] New way to do it

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- script that lists all genres from hbtn_0d_tvshows
 --  and displays the number of shows linked to each.
 SELECT g.name FROM tv_shows AS tv_s
-JOIN tv_show_genres AS tv_g ON tv_s.id = tv_g.show_id
-JOIN tv_genres AS g ON tv_g.genre_id = g.id
+INNER JOIN tv_show_genres AS tv_g ON tv_s.id = tv_g.show_id
+INNER JOIN tv_genres AS g ON tv_g.genre_id = g.id
 WHERE tv_s.title='Dexter';
