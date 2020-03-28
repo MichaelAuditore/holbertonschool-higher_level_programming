a84645af956df15c002236cd5270cd1fa2922d91
MichaelAuditore <michael_v613@hotmail.com>
2020-03-28 13:32:22 -0500

    [Fix] Execution statement

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
+    session = Session()
     query = session.query(State).limit(1)
     if query:
         print("{}: {}".format(query.id, query.name))
