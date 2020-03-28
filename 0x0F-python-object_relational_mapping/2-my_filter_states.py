0ee33850d5b200434490030655991ca3a2cefff4
MichaelAuditore <michael_v613@hotmail.com>
2020-03-28 12:57:06 -0500

    [Fix] Closing db

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,14 +3,15 @@
 if __name__ == "__main__":
     import MySQLdb as sql
     import sys
-    if len(sys.argv) == 5:
-        db = sql.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-        c = db.cursor()
-        c.execute("""
-        SELECT * FROM states WHERE name=%s ORDER BY id""", (sys.argv[4],))
-        for row in c.fetchall():
-            print(row)
+    db = sql.connect(host="localhost",
+                     port=3306,
+                     user=sys.argv[1],
+                     passwd=sys.argv[2],
+                     db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""
+    SELECT id, name FROM states WHERE name=%s ORDER BY id""", (sys.argv[4],))
+    for row in c.fetchall():
+        print(row)
+    c.close()
+    db.close()
