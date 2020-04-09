ac198749f5d798a93d3384be1ca45b26f880bea9
MichaelAuditore <michael_v613@hotmail.com>
2020-04-09 12:28:16 -0500

    [Fix] Silent response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send a post request with params
-curl -P -G "$1" -d 'email=hr@holbertonschool.com' -d 'subject=I%20will%20always%20be%20here%20for%20PLD'
+curl -sP -G "$1" -d 'email=hr@holbertonschool.com' -d 'subject=I%20will%20always%20be%20here%20for%20PLD'
