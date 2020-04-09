6cabe56799159bd5712c54e137539828fcd011dd
MichaelAuditore <michael_v613@hotmail.com>
2020-04-09 13:44:03 -0500

    [Fixed] Find a peak with lowest complexity algorithn Time Complexity algorithm was correctly written

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-""" module to find peak in a list of integers """
+""" find the peak
+"""
+
+
+def rec_peak(lbegin, last, l):
+    """ search peak into list of integers  """
+    mid = (lbegin + last)
+
+    if (mid % 2 == 0):
+        m = int(mid / 2)
+    else:
+        m = int((mid - 1) / 2)
+    if (m == 0 or l[m] >= l[m - 1]) and (m == last - 1 or l[m] >= l[m + 1]):
+        return (l[m])
+    elif (m > 0 and l[m] < l[m - 1]):
+        return (rec_peak(lbegin, m, l))
+    elif (m < last - 1 and l[m] < l[m + 1]):
+        return (rec_peak(m, last, l))
+    else:
+        return(None)
 
 
 def find_peak(list_of_integers):
-    """find peak in a list of integers"""
-    if list_of_integers is None or len(list_of_integers) == 0:
+    """ function that get the peak from a list of integers"""
+    if not list_of_integers:
         return (None)
-    else:
-        p = list_of_integers[0]
-        for number in list_of_integers:
-            if number > p:
-                p = number
-        return p
+
+    l = list_of_integers
+    return (rec_peak(0, len(l), l))
