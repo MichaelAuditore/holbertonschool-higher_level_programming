689f71eaa1b5db9ed0ee4b7afc00a0e5beff7345
MichaelAuditore <michael_v613@hotmail.com>
2020-04-15 12:18:16 -0500

    [Fix] URL doesn't passed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,12 +6,10 @@ if __name__ == "__main__":
     import sys
     import requests
 
-    data = {}
+    data = {'q': ""}
     if (len(sys.argv) > 1):
-        data["q"] = sys.argv[1]
-    else:
-        data["q"] = ""
-    r = requests.post(sys.argv[1], data=data)
+        data['q'] = sys.argv[1]
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
     try:
         json = r.json()
         if (len(js) == 0):
