6d7694abe5883cf90917111484c928300baf69b4
MichaelAuditore <michael_v613@hotmail.com>
2020-04-15 12:23:14 -0500

    [Fix] referencing right variable to check length and get values

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     r = requests.post("http://0.0.0.0:5000/search_user", data=send)
     try:
         json = r.json()
-        if (len(js) == 0):
+        if (len(json) == 0):
             print("No result")
         else:
-            print("[{}] {}".format(js.get('id'), js.get('name')))
+            print("[{}] {}".format(json.get('id'), json.get('name')))
     except:
         print("Not a valid JSON")
