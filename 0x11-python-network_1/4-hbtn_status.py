ea4dfb6f6e12bbae60308de70d293be56b7cf1b4
MichaelAuditore <michael_v613@hotmail.com>
2020-04-15 12:07:59 -0500

    [Fix] PEP8 style

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     import requests
 
     req = requests.get("https://intranet.hbtn.io/status")
-    print ("Body response:\n\t- type: {}\n\t- content: {}".format(
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(
         type(req.text), req.text))
