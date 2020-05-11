42963a6d5b941e3aeae2b8a5f00255e9754d20a0
MichaelAuditore <michael_v613@hotmail.com>
2020-05-11 12:32:47 -0500

    [Fix] Creates first an empty dict with values

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 /* Function to import a dictionary and print sort dictionary by user_id */
 const dict = require('./101-data').dict;
-const newDict = {};
-let last = 0;
-for (const [, value] of Object.entries(dict)) {
-  const arr = [];
-  for (const [key, value] of Object.entries(dict)) {
-    if (last === value) {
-      arr.push(key);
-      newDict[value] = arr;
+const listValues = new Set(Object.values(dict));
+const result = {};
+/* fill the dictionary with empty list for each key */
+listValues.forEach(element => {
+  result[element] = [];
+});
+for (const key1 of Object.keys(result)) {
+  for (const key2 of Object.keys(dict)) {
+    if (dict[key2] === parseInt(key1)) {
+      result[key1].push(key2);
     }
   }
-  last = value;
 }
-console.log(newDict);
+console.log(result);
