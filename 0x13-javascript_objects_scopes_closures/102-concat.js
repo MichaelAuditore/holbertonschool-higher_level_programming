b6826a6af9a186ee7eb2fbfc91b77ea84a2dad33
MichaelAuditore <michael_v613@hotmail.com>
2020-05-11 12:59:08 -0500

    [Fix] Shebang, and logic

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,15 +1,10 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 /* Concat files script with JS */
 const args = process.argv.slice(2);
-var path = require('path');
 var fs = require('fs');
 
-const path1 = path.resolve(args[0]);
-const path2 = path.resolve(args[1]);
-const destination = path.resolve(args[2]);
+const content = fs.readFileSync(args[0], 'utf-8');
+const content2 = fs.readFileSync(args[1], 'utf-8');
 
-const content = fs.readFileSync(path1);
-const content2 = fs.readFileSync(path2);
-
-fs.writeFileSync(destination, content + content2);
+fs.writeFileSync(args[2], (content + content2));
