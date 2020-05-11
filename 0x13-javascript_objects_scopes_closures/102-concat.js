9300dc4a22679f86b04db460bc1b4360004b167e
MichaelAuditore <michael_v613@hotmail.com>
2020-05-11 13:01:18 -0500

    [Fix variable name]

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,4 +5,4 @@ const fs = require('fs');
 const first = fs.readFileSync(process.argv[2], 'utf8');
 const second = fs.readFileSync(process.argv[3], 'utf8');
 
-result.writeFileSync(process.argv[4], (first + second));
+fs.writeFileSync(process.argv[4], (first + second));
