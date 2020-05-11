a5ede247741901af4bdd04435af94c4fd232a83c
MichaelAuditore <michael_v613@hotmail.com>
2020-05-11 11:09:06 -0500

    [New file] Class Square inherits from Rectangle, and printChar method

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* class Square inherits from Rectangle */
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* class Square inherits from Rectangle */
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  /* charPrint prints a square with a char passed to the function */
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        process.stdout.write('\n');
+      }
+    } else {
+      this.print();
+    }
+  }
+};
