e851bfe65f2ebe8a53aac64ce3729d4d55e4bba2
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 15:06:04 -0500

    [Fix] In case of no error when make request and an integer value is invalid return the last chapter title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,11 @@ const request = require('request');
 
 const page = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(page, function (error, status, body) {
-  if (error) {
+  if (status.statusCode == 200) {
+    console.log(JSON.parse(body).title);
+  } else if (error) {
     console.log(error);
+  } else {
+    console.log('The Force Awakens');
   }
-  console.log(JSON.parse(body).title);
 });
