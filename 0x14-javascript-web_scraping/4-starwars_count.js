ca1a77e1269bb8e21599bdae78efdebd635c01a4
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 16:10:55 -0500

    [Foreach loop into JSON body]

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,22 +2,20 @@
 /* Script to do a request to STAR WARS API
  and get the number of times a character with id 18 appears in movies */
 const request = require('request');
-let times = 0;
 const URL = process.argv[2];
-const expected = 'https://swapi-api.hbtn.io/api/films';
-if (URL === expected) {
-  request(URL, function (error, status, body) {
-    if (error) {
-      console.error(error);
-    }
-    const lista = JSON.parse(body);
-    for (let i = 0; i < lista.results.length; i++) {
-      for (let j = 0; j < lista.results[i].characters.length; j++) {
-        if (lista.results[i].characters[j].search('/18/') > 0) {
-          times += 1;
+request(URL, function (error, status, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body);
+    let times = 0;
+    for (const film of films.results) {
+      for (const character of film.characters) {
+        if (character.search('18') > 0) {
+          times++;
         }
       }
     }
     console.log(times);
-  });
-}
+  }
+});
