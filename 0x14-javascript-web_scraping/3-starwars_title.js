8705f987b5207569c41dd340cd5954e89e5ea985
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 15:07:18 -0500

    [Fix] Equal comparison operator

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const page = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(page, function (error, status, body) {
-  if (status.statusCode == 200) {
+  if (status.statusCode === 200) {
     console.log(JSON.parse(body).title);
   } else if (error) {
     console.log(error);
