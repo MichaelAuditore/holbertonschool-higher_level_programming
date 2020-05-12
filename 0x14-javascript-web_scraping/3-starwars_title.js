6067e536406e1d4c3965209c21b1b7033e56037e
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 14:50:53 -0500

    [Fix] print an error into log

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const request = require('request');
 const page = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(page, function (error, status, body) {
   if (error) {
-    console.error(error);
+    console.log(error);
   }
   console.log(JSON.parse(body).title);
 });
