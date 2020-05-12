542ba65fc59672e56a3d874d2011fce51456abe3
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 14:39:09 -0500

    [New logic] New variable to get arg and new way to iterate between elements of JSON

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
 /* Script to do a request to STAR WARS API
  and get the number of times a character with id 18 appears in movies */
-const REQUEST = require('request');
-REQUEST(process.argv[2], function (err, resp, body) {
-  if (err) {
-    console.error(err);
-  }
-  let times = 0;
-  const MOVIES = JSON.parse(body).results;
-  for (let i = 0; i < MOVIES.length; i++) {
-    for (let j = 0; j < MOVIES[i].characters.length; j++) {
-      if (MOVIES[i].characters[j].search('/18/') > 0) {
-        times++;
-      }
+const request = require('request');
+let times = 0;
+const URL = process.argv[2];
+request(URL, function (error, status, body) {
+    if (error) {
+	console.error(error);
     }
-  }
-  console.log(times);
+    const lista = JSON.parse(body);
+    for (let i = 0; i < lista.results.length; i++) {
+	for (let j = 0; j < lista.results[i].characters.length; j++) {
+	    if (lista.results[i].characters[j].search('/18/') > 0) {
+		times += 1;
+	    }
+	}
+    }
+    console.log(times);
 });
