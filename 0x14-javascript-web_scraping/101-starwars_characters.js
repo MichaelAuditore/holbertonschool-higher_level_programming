8cd18b18b9b1f9dbbab62fc4b9993574dc3c1aa1
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 17:36:21 -0500

    [Fix] New function to create a dict of names and print in right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
-/* Script to do a request to STAR WARS API
- and get the characters  */
+// print all character names of a SW movie got it by id
 const request = require('request');
 const URL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(URL, function (error, status, body) {
+let characters;
+const dict = {};
+request(URL, function (error, response, body) {
   if (error) {
     console.error(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, status, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+  }
+  characters = JSON.parse(body).characters;
+  for (const url of characters) {
+    request(url, (error, response, body) =>
+      !error && addNames(url, JSON.parse(body).name));
   }
 });
+
+function addNames (url, name) {
+  dict[url] = name;
+  if (Object.entries(dict).length === characters.length) {
+    for (const url of characters) {
+      console.log(dict[url]);
+    }
+  }
+}
