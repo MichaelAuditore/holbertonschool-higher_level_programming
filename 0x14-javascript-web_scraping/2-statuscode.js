7f6ee382e0ee51eee73459c7c5b9111d069c8ecf
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 14:34:38 -0500

    [Fix] Output Fixed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 /* Script that prints status of a request. */
 const request = require('request');
 request.get(process.argv[2]).on('response', function (response) {
-  console.log(response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
