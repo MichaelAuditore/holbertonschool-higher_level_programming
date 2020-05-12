b85853f6058cbcaa4f90751fa46d8cedfb08ae7c
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 16:31:36 -0500

    [Fix] Conditional statement added to check when get dict

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,16 +6,17 @@ const URL = process.argv[2];
 request(URL, function (error, status, body) {
   if (error) {
     console.error(error);
-  }
-  const dict = {};
-  const tasks = JSON.parse(body);
-  for (let i = 0; i < tasks.length; i++) {
-    dict[tasks[i].userId] = 0;
-  }
-  for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].completed) {
-      dict[tasks[i].userId] += 1;
+  } else {
+    const dict = {};
+    const tasks = JSON.parse(body);
+    for (let i = 0; i < tasks.length; i++) {
+      dict[tasks[i].userId] = 0;
     }
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed) {
+        dict[tasks[i].userId] += 1;
+      }
+    }
+    console.log(dict);
   }
-  console.log(dict);
 });
