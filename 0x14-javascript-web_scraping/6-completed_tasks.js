ca3029216976272056c005e22573430a7a4e8168
MichaelAuditore <michael_v613@hotmail.com>
2020-05-12 16:29:40 -0500

    [New Script] Print a dictionary with key refers a userId and value the number of tasks did it by user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* Script to do a request to STAR WARS API
+ and get the number of times a character with id 18 appears in movies */
+const request = require('request');
+const URL = process.argv[2];
+request(URL, function (error, status, body) {
+  if (error) {
+    console.error(error);
+  }
+  const dict = {};
+  const tasks = JSON.parse(body);
+  for (let i = 0; i < tasks.length; i++) {
+    dict[tasks[i].userId] = 0;
+  }
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].completed) {
+      dict[tasks[i].userId] += 1;
+    }
+  }
+  console.log(dict);
+});
