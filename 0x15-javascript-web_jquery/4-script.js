e7c808c0c26aadecdb1035cc86af92f9e45cfc7b
MichaelAuditore <michael_v613@hotmail.com>
2020-05-14 11:03:09 -0500

    [Fix] Semistandard code

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,7 +1,7 @@
-let div = $('#toggle_header');
+const div = $('#toggle_header');
 div.click(function () {
-    let header = $('header');
-    let clss = header.attr('class');
-    header.addClass(clss === 'red' ? 'green' : 'red');
-    header.removeClass(clss === 'red' ? 'red' : 'green');
+  const header = $('header');
+  const clss = header.attr('class');
+  header.addClass(clss === 'red' ? 'green' : 'red');
+  header.removeClass(clss === 'red' ? 'red' : 'green');
 });
